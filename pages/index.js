59c1b3e7baa1ff833ff84c3ade9ce5a0d1a1201e
ethanfrydenlund <ethan.frydenlund@gmail.com>
2023-03-04 16:16:09 -0600

    tried my best

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,11 @@ import styles from '@/styles/Home.module.css'
 export default function Home() {
   return (
     <>
-      <div id = {styles.wrapper}>
-        <div id = {styles.pantry}></div>
-        <div id = {styles.personal_info}></div>
-        <div id = {styles.meal_section}></div>
-        <div id = {styles.summary}></div>
+      <div className = {styles.wrapper}>
+        <div className = {styles.pantry}></div>
+        <div className = {styles.personal_info}></div>
+        <div className = {styles.meal_section}></div>
+        <div className = {styles.summary}></div>
       </div>
     </>
   )
