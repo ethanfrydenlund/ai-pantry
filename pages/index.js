9864e40d26d2c5ee5499027f2b756237c8755c8e
ethanfrydenlund <ethan.frydenlund@gmail.com>
2023-03-04 16:13:40 -0600

    fixed style ref

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
+import styles from '@/styles/Home.module.css'
 export default function Home() {
   return (
-    <div id = "wrapper">
-      <div id = "pantry"></div>
-      <div id = "personal_info"></div>
-      <div id = "meal_section"></div>
-      <div id = "summary"></div>
-    </div>
+    <>
+      <div id = {styles.wrapper}>
+        <div id = {styles.pantry}></div>
+        <div id = {styles.personal_info}></div>
+        <div id = {styles.meal_section}></div>
+        <div id = {styles.summary}></div>
+      </div>
+    </>
   )
 }
