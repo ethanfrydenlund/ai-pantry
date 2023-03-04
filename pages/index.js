70976739fdda981f40efc124c17b025e7a6d6ab5
ethanfrydenlund <ethan.frydenlund@gmail.com>
2023-03-04 15:52:05 -0600

    adding divs

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,11 @@ import styles from '@/styles/Home.module.css'
 
 export default function Home() {
   return (
-    <>
+    <div id = "wrapper">
       <div id = "pantry"></div>
       <div id = "personal_info"></div>
       <div id = "meal_section"></div>
       <div id = "summary"></div>
-    </>
+    </div>
   )
 }
