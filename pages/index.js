1e9890e8efe3f09c923f48f70fd77a03603e0882
ethanfrydenlund <ethan.frydenlund@gmail.com>
2023-03-04 17:04:56 -0600

    added ingredient form

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ export default function Home() {
     <>
       <div className = {styles.wrapper}>
         <div className = {styles.pantry}></div>
+          <form className = {styles.getIngredient} value = "value"></form>
         <div className = {styles.personal_info}></div>
         <div className = {styles.summary}></div>
         <div className = {styles.meal_section}></div>
