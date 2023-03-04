28291176a5beebf692499408eb3ac8d9edbc1069
ethanfrydenlund <ethan.frydenlund@gmail.com>
2023-03-04 15:40:26 -0600

    hopefully it adds

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import styles from '@/styles/Home.module.css'
 export default function Home() {
   return (
     <>
+      <div id = "pantry"></div>
+      <div id = "personal_info"></div>
+      <div id = "meal_section"></div>
+      <div id = "summary"></div>
     </>
   )
 }
