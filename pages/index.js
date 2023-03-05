a618dd40d0feaae2e1966e5f2ccf0e90c5a8eeb4
ethanfrydenlund <ethan.frydenlund@gmail.com>
2023-03-04 19:18:32 -0600

    idek :(

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import styles from '@/styles/Home.module.css'
 export default function Home() {
+  function handleIngredient(){
+    var input = document.getElementById("ingredient");
+    var inputVal = input.value;
+    var pantry = document.getElementById("pantry");
+    var text = document.createElement("p");
+    text.textContent = inputVal;
+    pantry.appendChild(text);
+  }
   return (
     <>
       <div className = {styles.wrapper}>
-        <div className = {styles.pantry}></div>
-          <form className = {styles.getIngredient} value = "value"></form>
+        <div className = {styles.pantry} id = "pantry">
+          <div className = {styles.ingredient_entry}>
+            <input type="text" id = "ingredient"></input>
+            <button className = {styles.normal_button} onClick = {handleIngredient}></button>
+          </div>
+        </div>
         <div className = {styles.personal_info}></div>
         <div className = {styles.summary}></div>
         <div className = {styles.meal_section}></div>
