da93a26a9819b9bd1294d754f089d473907cd62d
ethanfrydenlund <ethan.frydenlund@gmail.com>
2023-03-05 07:54:24 -0600

    fixed the deleting?

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,13 @@ export default function Home() {
   function handleIngredient(){
     var input = document.getElementById("ingredient");
     var inputVal = input.value;
-    ReactDOM.render(< Ingredient_item name={inputVal}/>, document.getElementById("pantry"));
+    ReactDOM.render(< Ingredient_item name={inputVal}/>, document.getElementById("pantryEntries"));
   }
   return (
     <>
       <div className = {styles.wrapper}>
         <div className = {styles.pantry} id = "pantry">
+          <div id = "pantryEntries"></div>
           <div className = {styles.ingredient_entry}>
             <input type="text" id = "ingredient"></input>
             <button className = {styles.normal_button} onClick = {handleIngredient}></button>
